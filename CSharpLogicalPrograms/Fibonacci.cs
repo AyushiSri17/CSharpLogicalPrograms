fefd44f9055056a791bcd06baaa9be3267b284b2
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-01 18:36:59 +0530

    [Ayushi] Added Fibonacci Series Program

---
+++ CSharpLogicalPrograms/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLogicalPrograms
+{
+    public class Fibonacci
+    {
+        int firstNum = 0, secondNum = 1, thirdNum=0;
+        public void FibonacciSerirs(int n)
+        {
+            Console.WriteLine("Fibonacci Series: ");
+            while (n>0)
+            {
+                Console.Write(firstNum);
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                n--;
+            }
+        
+        }
+
+    }
+}
--- CSharpLogicalPrograms/Program.cs
+++ CSharpLogicalPrograms/Program.cs
@@ -11,6 +11,23 @@ namespace CSharpLogicalPrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# logical Programs");
+
+            Console.WriteLine("Enter the option to perform perticular program");
+            Console.WriteLine("1. Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number to terms want to print");
+                    int term= Convert.ToInt32(Console.ReadLine());
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSerirs(term);
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
