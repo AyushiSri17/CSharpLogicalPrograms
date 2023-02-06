654d6cce5093baf4f4117c26f380b2010f9a1c0a
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-06 21:02:57 +0530

    [Ayushi] Added Coupon Number Program

---
+++ CSharpLogicalPrograms/CouponNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLogicalPrograms
+{
+    public class CouponNumber
+    {
+        public void Coupon()
+        {
+            Console.WriteLine("Enter the number of coupons want to generate");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            int[] discoupon = new int[n];
+            Random r = new Random();
+            int count = 0;
+
+            while (i < n)
+            {
+                int random = r.Next(10,100);
+                Console.WriteLine(random);
+                count++;
+                if (discoupon.Contains(random))
+                    //skip that particular iteration
+                    continue;
+                else
+                    discoupon[i] = random;
+                i++;
+            }
+            Console.WriteLine("Distinct coupon numbers are");
+            foreach (int coupon in discoupon)
+            {
+                Console.Write(coupon + " ");
+            }
+            Console.WriteLine("From {1} random numbers {0} distinct number coupons are generated",n,count);
+
+        }
+    }
+}
--- CSharpLogicalPrograms/Program.cs
+++ CSharpLogicalPrograms/Program.cs
@@ -13,7 +13,7 @@ namespace CSharpLogicalPrograms
             Console.WriteLine("Welcome to C# logical Programs");
 
             Console.WriteLine("Enter the option to perform perticular program");
-            Console.WriteLine("1. Fibonacci Series \n2. Perfect Number \n3. Prime Number \n4. Reverse Number");
+            Console.WriteLine("1. Fibonacci Series \n2. Perfect Number \n3. Prime Number \n4. Reverse Number \n5.Coupon Number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -34,6 +34,10 @@ namespace CSharpLogicalPrograms
                 case 4:
                     ReverseNumber reverse= new ReverseNumber();
                     reverse.GetReverseNumber();
+                    break;
+                case 5:
+                    CouponNumber coupon= new CouponNumber();
+                    coupon.Coupon();
                     break;
                 default:
                     Console.WriteLine("Enter the correct option");
